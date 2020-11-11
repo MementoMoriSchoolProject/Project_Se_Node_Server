7a660c8e102170c53f21658bc4c9935b2b8b66aa
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-11-11 21:50:34 +0100

    entity

---
+++ src/entities/buryCremation.ts
@@ -0,0 +1,87 @@
+import { ObjectType, Field } from 'type-graphql';
+import { prop as Property } from '@typegoose/typegoose';
+
+enum buryCremation {
+    bury,
+    cremation
+}
+
+@ObjectType({ description: 'The buryCremation model' })
+export class BuryCremation {
+
+    @Field({ nullable: true })
+    @Property()
+    buryCremation?: buryCremation;
+
+    @Field(() => Date, { nullable: true })
+    @Property()
+    date?: Date;
+
+    @Field(() => Date, { nullable: true })
+    @Property()
+    timeOfArrival?: Date;
+
+    @Field({ nullable: true })
+    @Property()
+    namePlace?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    address?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    postalCode?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    location?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    speakerOrder?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    specialNeeds?: string;
+
+    // start of cremation 
+    @Field({ nullable: true })
+    @Property()
+    cascetView?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    insertion ?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    ashesDestination?: string;
+    // end of cremation
+
+    // start of bury 
+    @Field({ nullable: true })
+    @Property()
+    kindOfGrave?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    existingGrave?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    sectionNumber?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    burriedRecently?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    cascetDescend?: string;
+
+    @Field({ nullable: true })
+    @Property()
+    stonemason?: string;
+    // end of bury
+}
