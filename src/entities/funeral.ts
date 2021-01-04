625414ba46c0b06f2aeaac049fda2c6b24e0f27e
Joey <pothofj@hva.nl>
2021-01-04 22:10:47 +0100

    feat: advertisement page

--- src/entities/funeral.ts
+++ src/entities/funeral.ts
@@ -15,6 +15,7 @@ import { CommemorativeCard } from './commemorativecard';
 import { Ceremony } from './ceremony';
 import { BuryCremation } from './buryCremation';
 import { Nightguard } from './nightguard';
+import { Advertisement } from './advertisement';
 
 @ObjectType({ description: 'The funeral model' })
 export class Funeral {
@@ -44,6 +45,10 @@ export class Funeral {
     @Field(_type => [Visiting], { nullable: true })
     @Property({ type: [Visiting], required: false })
     visiting?: Visiting[];
+
+    @Field(_type => [Advertisement], { nullable: true })
+    @Property({ type: [Advertisement], required: false })
+    advertisement?: Advertisement[];
 
     @Field(_type => FinalCare, { nullable: true })
     @Property({ type: FinalCare, required: false })
