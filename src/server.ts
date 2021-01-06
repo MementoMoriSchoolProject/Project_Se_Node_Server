9948326463bb315a25dba7c2906d944fd385e69c
Ziggy <31688986+ZIggyVeerman@users.noreply.github.com>
2021-01-06 10:38:20 +0100

    Update server.ts
    comma

--- src/server.ts
+++ src/server.ts
@@ -61,7 +61,7 @@ const main = async () => {
             NightguardResolver,
             TransportResolver,
             DrivingInfoFuneralCarResolver,
-            DrivingInfoFollowingCarResolver
+            DrivingInfoFollowingCarResolver,
             CascetResolver,
             AdvertisementResolver,
             LayoutResolver,
